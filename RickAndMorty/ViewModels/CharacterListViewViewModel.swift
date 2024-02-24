979b79be853667313e5b4b7bd95d2ebe1824bbad
Dmitrii642 <dmit.iakovlev@gmail.com>
2024-02-24 20:09:48 +0300

    Character list view updates

---
+++ RickAndMorty/ViewModels/CharacterListViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterListViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 24.02.2024.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/CharacterListView.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterListView.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 24.02.2024.
+//
+
+import UIKit
+
+class CharacterListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
