675343dcf1faa1f9c409efc8a95deb9d2e4155f4
Dmitrii642 <dmit.iakovlev@gmail.com>
2024-02-25 18:12:24 +0300

    Create character cell

---
+++ RickAndMorty/ViewModels/CharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CharacterCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 25.02.2024.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/CharacterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 25.02.2024.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    
+}
