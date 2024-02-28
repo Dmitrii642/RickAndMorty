6ffd355dc5087e220ad05971b45531e24e655d6e
Dmitrii642 <dmit.iakovlev@gmail.com>
2024-02-28 19:03:45 +0300

    Add a pagination activity indicator at the bottom of collection View

---
+++ RickAndMorty/Views/FooterLoadingCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  FooterLoadingCollectionReusableView.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 28.02.2024.
+//
+
+import UIKit
+
+class FooterLoadingCollectionReusableView: UICollectionReusableView {
+        
+}
