85c5f902cc2e6a17cf42a66f63a9583bbcb30884
Dmitrii642 <dmit.iakovlev@gmail.com>
2024-03-07 17:23:58 +0300

    Add a caching mechanism for API responses

---
+++ RickAndMorty/Views/EpisodeDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  EpisodeDetailView.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 07.03.2024.
+//
+
+import UIKit
+
+class EpisodeDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
