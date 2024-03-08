5b03cc8a12bb89158565d8fcbff6df2a39fae35a
Dmitrii642 <dmit.iakovlev@gmail.com>
2024-03-08 18:48:05 +0300

    Building out the episode tab list view

---
+++ RickAndMorty/Models/APIResponseTypes/GetAllEpisodesResponse.swift
@@ -0,0 +1,8 @@
+//
+//  GetAllEpisodesResponse.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 08.03.2024.
+//
+
+import Foundation
---
+++ RickAndMorty/ViewModels/EpisodeListViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EpisodeListViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 07.03.2024.
+//
+
+import Foundation
