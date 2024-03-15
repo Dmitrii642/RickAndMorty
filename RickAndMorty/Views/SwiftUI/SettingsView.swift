e23a19368fb1357b2db5e74db05a23f694eb6545
Dmitrii642 <dmit.iakovlev@gmail.com>
2024-03-15 12:27:16 +0300

    Update setting screen by SwiftUI

---
+++ RickAndMorty/Views/SwiftUI/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 15.03.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
