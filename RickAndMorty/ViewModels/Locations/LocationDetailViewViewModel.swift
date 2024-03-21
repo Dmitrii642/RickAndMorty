d1f9dfbb92ad435b4f1ec8709c3b9b5449ed0d1d
Dmitrii642 <dmit.iakovlev@gmail.com>
2024-03-21 19:42:29 +0300

    Update location details screen

---
+++ RickAndMorty/ViewModels/Locations/LocationDetailViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LocationDetailViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 21.03.2024.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/LocationDetails/LocationDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  LocationDetailView.swift
+//  RickAndMorty
+//
+//  Created by Dmitrii Iakovlev on 21.03.2024.
+//
+
+import Foundation
